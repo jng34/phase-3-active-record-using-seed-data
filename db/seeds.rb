dde11165a056b71a7099b77f6ef66ca270207e72
JamesNg <njames15@gmail.com>
2022-06-12 18:03:41 -0400

    commit tree 6d99fafd03147807c5ee78f0c7f582e1dc58d866

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,19 @@
 # Add a console message so we can see output when the seed file runs
+puts "Seeding games..."
+
+50.times do 
+    Game.create(
+        title: Faker::Game.title,
+        genre: Faker::Game.genre,
+        platform: Faker::Game.platform,
+        price: rand(0..60)
+    )
+end
+
+puts "Done seeding!"
+
+# Game.create(title: "StarCraft", platform: "Blizzard", genre: "MMP", price: 30)
+# Game.create(title: "Breath of the Wild", platform: "Switch", genre: "Action-adventure", price: 60)
+# Game.create(title: "Final Fantasy VII", platform: "Playstation", genre: "RPG", price: 60)
+# Game.create(title: "Mario Kart", platform: "Switch", genre: "Racing", price: 60)
+# Game.create(title: "Candy Crush Saga", platform: "Mobile", genre: "Puzzle", price: 0)
